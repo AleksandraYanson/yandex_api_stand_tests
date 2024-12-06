659fea1795aad3b6fa197c87475e49ed061d07b9
Aleksandra Yanson <ijedark@gmail.com>
2024-12-06 13:49:15 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,10 @@
+URL_SERVICE = "https://253eef2a-364d-497c-baf8-a16e7d1111e4.serverhub.praktikum-services.ru"
+
+# DOC_PATH содержит путь к документации веб-сервиса.
+# Этот путь используется для формирования полного URL пути к документации, добавляя его к базовому URL сервиса.
+# В результате получится что-то вроде "https://api.example.com/docs/"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
